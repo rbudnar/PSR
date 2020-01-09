16d8c503b8e2ce28147550cb1ab9223b39e56964
Ronald Budnar <budnarr@gmail.com>
2020-01-08 22:40:25 -0600

    Refactor and cleanup converter

--- convert.py
+++ convert.py
@@ -6,92 +6,16 @@ import matplotlib.pyplot as plt
 from os import listdir, system, mkdir
 from os.path import isfile, join
 
-
-def get_pixel_count(path, save_files=False):
-    img = cv2.imread(path, cv2.COLOR_BGR2RGB)
-
-    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    red_mask = create_mask(img_hsv, ['red'])
-    mask_img = cv2.bitwise_and(img_hsv, img_hsv, mask=red_mask)
-    gray_img = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
-
-    red_pixel_area = cv2.countNonZero(gray_img)
-    white_mask = create_mask(img_hsv, ['white'])
-    white_mask_img = cv2.bitwise_and(img_hsv, img_hsv, mask=white_mask)
-    white_mask_img_gray = cv2.cvtColor(white_mask_img, cv2.COLOR_BGR2GRAY)
-    non_tissue_area = cv2.countNonZero(white_mask_img_gray)
-    total_area = gray_img.shape[0] * gray_img.shape[1]
-    percentage = red_pixel_area / (total_area - non_tissue_area)
-
-    fig, axs = plt.subplots(3, 2, figsize=(18, 18))
-    fig.suptitle(f"Image: {path}", fontsize=18, fontweight='bold')
-    fig.text(0.05, 0.05,
-             f"TOTAL PIXELS: {total_area}; RED PIXELS: {red_pixel_area}; NON-TISSUE PIXELS: {non_tissue_area}; PERCENT RED: {percentage:f}",
-             fontsize=14)
-    axs[0, 0].imshow(rgb_img)
-    axs[0, 0].set_title("Original image")
-    axs[0, 1].imshow(img_hsv)
-    axs[0, 1].set_title("HSV image")
-    axs[1, 0].imshow(mask_img)
-    axs[1, 0].set_title("HSV image with red mask")
-    axs[1, 1].imshow(gray_img, cmap='gray')
-    axs[1, 1].set_title("Converted image for pixel count")
-    axs[2, 0].imshow(white_mask_img)
-    axs[2, 0].set_title("Non-tissue mask")
-    axs[2, 1].imshow(white_mask_img_gray, cmap='gray')
-    axs[2, 1].set_title("Non-tissue image for pixel count")
-
-    if save_files:
-        i = path.rindex(".")
-        foldername = path[:i]
-        mkdir(foldername)
-        plt.savefig(f"./{foldername}/{path}_plot.tif")
-        save_img(img, f"{path}_original.tif", foldername)
-        save_img(cv2.cvtColor(img_hsv, cv2.COLOR_BGR2RGB),
-                 f"{path}_hsv.tif", foldername)
-        save_img(cv2.cvtColor(mask_img, cv2.COLOR_BGR2RGB),
-                 f"{path}_red_mask.tif", foldername)
-        save_img(gray_img, f"{path}_red_mask_count.tif", foldername)
-        save_img(cv2.cvtColor(white_mask_img, cv2.COLOR_BGR2RGB),
-                 f"{path}_white_mask.tif", foldername)
-        save_img(white_mask_img_gray,
-                 f"{path}_white_mask_count.tif", foldername)
-
-        white_mask_img_gray
-
-    plt.close()
-
-    return (red_pixel_area, non_tissue_area, total_area, percentage)
-
-
-def create_mask(hsv_img, colors):
-    """
-    Creates a binary mask from HSV image using given colors.
-    """
-    mask = np.zeros((hsv_img.shape[0], hsv_img.shape[1]), dtype=np.uint8)
-
-    for color in colors:
-        for color_range in HSV_RANGES[color]:
-            mask += cv2.inRange(
-                hsv_img,
-                color_range['lower'],
-                color_range['upper']
-            )
-
-    return mask
-
-
-def clear():
-    system('cls')
-
-
-def save_img(img, img_name, path):
-    cv2.imwrite(f"./{path}/{img_name}", img)
-
+RED_MASK = "red_mask"
+RED_MASK_COUNT = "red_mask_count"
+WHITE_MASK = "white_mask"
+WHITE_MASK_COUNT = "white_mask_count"
+ORIGINAL = "original"
+HSV = "hsv"
 
 HSV_RANGES = {
     # red is a major color
+    # note that other shades of red were tested for analysis
     'red': [
         {
             'lower': np.array([154, 118, 106]),
@@ -172,16 +96,138 @@ HSV_RANGES = {
     ]
 }
 
-onlyfiles = [f for f in listdir("./") if f.endswith(".tif")]
-df = pd.DataFrame(columns=['filename', 'red_pixel_count',
-                           'non_tissue_pixel_count', 'total_pixel_count', 'percent_red'])
-filecount = len(onlyfiles)
-for (i, file) in enumerate(onlyfiles):
-    clear()
-    print(f"Processing {i}/{filecount}: {file}")
-    red_pixel_area, non_tissue_area, total_area, percentage = get_pixel_count(
-        file, True)
-    df.loc[i] = [file, red_pixel_area, non_tissue_area, total_area, percentage]
 
-df.to_csv("./PSR_results.csv")
-print("Done!")
+def get_pixel_count(path_to_img_dir, filename, save_files=False):
+    """
+    Generates pixel counts for red stained tissue, non tissue area, total area, and the resulting percentage.
+    Tissue area is quantified as total_area - non_tissue_area.
+    """
+    images = dict()
+    img = cv2.imread(f"./{path_to_img_dir}/{filename}")
+
+    images[ORIGINAL] = rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    images[HSV] = img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    mask_red = create_mask(img_hsv, ['red'])
+    images[RED_MASK] = red_mask = cv2.bitwise_and(
+        img_hsv, img_hsv, mask=mask_red)
+    images[RED_MASK_COUNT] = red_mask_count = cv2.cvtColor(
+        red_mask, cv2.COLOR_BGR2GRAY)
+    red_pixel_area = cv2.countNonZero(red_mask_count)
+
+    mask_white = create_mask(img_hsv, ['white'])
+    images[WHITE_MASK] = white_mask = cv2.bitwise_and(
+        img_hsv, img_hsv, mask=mask_white)
+    images[WHITE_MASK_COUNT] = white_mask_count = cv2.cvtColor(
+        white_mask, cv2.COLOR_BGR2GRAY)
+
+    non_tissue_area = cv2.countNonZero(white_mask_count)
+    total_area = red_mask_count.shape[0] * red_mask_count.shape[1]
+    percentage = red_pixel_area / (total_area - non_tissue_area)
+
+    return (red_pixel_area, non_tissue_area, total_area, percentage, images)
+
+
+def generate_plot(images, filename, total_area, red_pixel_area, non_tissue_area, percentage, path_to_new_folder, save_files):
+    """
+    Generates a plot with all 6 images used for analysis with resulting pixel counts and percentages.
+    """
+    fig, axs = plt.subplots(3, 2, figsize=(18, 18))
+    fig.suptitle(f"Image: {filename}", fontsize=18, fontweight='bold')
+    fig.text(0.05, 0.05,
+             f"TOTAL PIXELS: {total_area}; RED PIXELS: {red_pixel_area}; NON-TISSUE PIXELS: {non_tissue_area}; PERCENT RED: {percentage:f}",
+             fontsize=14)
+    axs[0, 0].imshow(images[ORIGINAL])
+    axs[0, 0].set_title("Original image")
+    axs[0, 1].imshow(images[HSV])
+    axs[0, 1].set_title("HSV image")
+    axs[1, 0].imshow(images[RED_MASK])
+    axs[1, 0].set_title("HSV image with red mask")
+    axs[1, 1].imshow(images[RED_MASK_COUNT], cmap='gray')
+    axs[1, 1].set_title("Converted image for pixel count")
+    axs[2, 0].imshow(images[WHITE_MASK])
+    axs[2, 0].set_title("Non-tissue mask")
+    axs[2, 1].imshow(images[WHITE_MASK_COUNT], cmap='gray')
+    axs[2, 1].set_title("Non-tissue image for pixel count")
+
+    if save_files:
+        plt.savefig(f"{path_to_new_folder}/{filename}_plot.tif")
+
+    plt.close()
+
+
+def save_images(images, path_to_new_folder, filename):
+    """
+    Persists generated images for review.
+    """
+    for (key, image) in images.items():
+        save_img(cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
+                 f"{filename}_{key}.tif", path_to_new_folder)
+
+
+def create_mask(hsv_img, colors):
+    """
+    Creates a binary mask from HSV image using given colors.
+    """
+    mask = np.zeros((hsv_img.shape[0], hsv_img.shape[1]), dtype=np.uint8)
+
+    for color in colors:
+        for color_range in HSV_RANGES[color]:
+            mask += cv2.inRange(
+                hsv_img,
+                color_range['lower'],
+                color_range['upper']
+            )
+
+    return mask
+
+
+def clear():
+    system('cls')
+
+
+def save_img(img, img_name, path):
+    cv2.imwrite(f"./{path}/{img_name}", img)
+
+
+def generate_path_to_new_folder(filename, path_to_img_dir):
+    i = filename.rindex(".")
+    foldername = filename[:i]
+    return f"./{path_to_img_dir}/{foldername}"
+
+
+def run(path_to_img_dir, image_format, save_files=True):
+    """
+    Runs the procedure to generate pixel counts for the given images.
+    """
+    # Grab files from specified directory
+    onlyfiles = [f for f in listdir(
+        path_to_img_dir) if f.endswith(image_format)]
+    # create Data frame for pixel stats results
+    df = pd.DataFrame(columns=['filename', 'red_pixel_count',
+                               'non_tissue_pixel_count', 'total_pixel_count', 'percent_red'])
+    filecount = len(onlyfiles)
+    for (i, filename) in enumerate(onlyfiles):
+        clear()
+        print(f"Processing {i}/{filecount}: {filename}")
+        red_pixel_area, non_tissue_area, total_area, percentage, images = get_pixel_count(
+            path_to_img_dir, filename)
+
+        path_to_new_folder = generate_path_to_new_folder(
+            filename, path_to_img_dir)
+
+        if save_files:
+            mkdir(path_to_new_folder)
+            save_images(images, path_to_new_folder, filename)
+
+        generate_plot(images, filename, total_area, red_pixel_area,
+                      non_tissue_area, percentage, path_to_new_folder, save_files)
+        # add results to dataframe
+        df.loc[i] = [filename, red_pixel_area,
+                     non_tissue_area, total_area, percentage]
+
+    df.to_csv("./PSR_results.csv")
+    print("Done!")
+
+
+run("./images", image_format=".tif", save_files=True)
